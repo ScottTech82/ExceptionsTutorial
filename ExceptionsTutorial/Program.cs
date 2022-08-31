24acd60f7fa2f3e1eb6ea2b0b608924c6762da3c
Josh Scott <joshscott82@gmail.com>
2022-08-30 20:13:18 -0400

    Add project files.

---
+++ ExceptionsTutorial/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            Method1();
+        } catch (Exception ex)
+        {
+            Console.WriteLine("Exception in Main");         
+        }
+    }
+    private static void Method1()
+    {
+        Method2();
+    }
+    private static void Method2()
+    {
+        Method3();
+     
+    }
+    private static void Method3()
+    {
+        int n = 0;
+        int d = 0;
+        int x = n / d;
+        
+    }
+    private static void method4()
+    {
+
+    }
+}
